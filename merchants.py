aee152222c30e59cedc7f0e8cbf6d0e3e0f7b6ac
Rauru <121907785+Andrew123456789012@users.noreply.github.com>
2024-04-24 18:06:38 -0400

    Added houses and icons

--- merchants.py
+++ merchants.py
@@ -8,12 +8,13 @@ def merchant_interaction(player):
     print(f"{Fore.BLUE}you have encountered a merchant table!")
     print(f"you have {player.gold} gold.")
     print("what would you like to buy?")
-    print("1. hammer (2 gold)")
-    print("2. pickaxe (3 gold)")
-    print("3. block (1 gold)")
-    print("4. heart (10 gold)")
-    print("5. heart slot (20 gold)")
-    print("6. exit")
+    print("1. hammer (2 gold)      --█")
+    print("2. pickaxe (3 gold)     --)")
+    print("3. block (1 gold)        X")
+    print("4. heart (10 gold)       ♡")
+    print("5. heart slot (20 gold)  ♡")
+    print("6. house (50 gold)       ♜")
+    print("7. exit                  ⏎")
     choice = input("enter your choice: ")
 
     if choice == "1" and player.gold > 1:
@@ -32,16 +33,20 @@ def merchant_interaction(player):
       player.health += 1
       player.gold -= 10
       print("you bought a heart!")
-    elif choice == "5" and player.gold > 19 and p.health <10:
+    elif choice == "5" and player.gold > 19 and player.max_health <10:
       player.max_health += 1
       player.health = player.max_health
       player.gold -= 20
       print("you bought a heart slot!")
-    elif choice == "6":
+    elif choice == "6" and player.gold > 49:
+      player.gold -= 50
+      player.special = "house"
+      print("you bought a house!")
+      print("next time you press e it you will place it")
+    elif choice == "7":
       print("you exit the merchant.")
       break
     else:
       print("invalid choice or not enough gold.")
-      input("press enter to continue.")
+    input("press enter to continue.")
   print(Fore.WHITE)
-
