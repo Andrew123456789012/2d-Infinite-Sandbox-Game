428758edbd831cf5f67dd92d1a69a4d1568c5278
Rauru <121907785+Andrew123456789012@users.noreply.github.com>
2024-05-13 13:07:10 -0400

    Prepared it to become the server for html

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+from blessed import Terminal
 import game
+import menu
 
-game.game()
+menu.menu()
+
+game_stats = game.game_start()
+
+i = game_stats[0]
+p = game_stats[1]
+u = game_stats[2]
+g = game_stats[3]
+w = game_stats[4]
+b = game_stats[5]
+
+game.update(p, w, i, b)
+
+while True:
+  with Terminal().cbreak():  #input detection and player movement
+    print("")
+    move = Terminal().inkey(timeout=60)
+  game.player_move(p, g, u, w, i, move)
+  game.update(p, w, i, b)
